e303405d5afe390fae2993725497ed339e4c6d3c
Trabajo <Trabajo@DESKTOP-7IV2284>
2025-05-21 00:34:25 -0500

    ghg

--- Semana6_rtipantuñatarea/Views/vEstudiante.xaml.cs
+++ Semana6_rtipantuñatarea/Views/vEstudiante.xaml.cs
@@ -33,7 +33,7 @@ public partial class vEstudiante : ContentPage
 
     private void lvEstudiantes_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
-       var objEstudiante = (Rol)e.SelectedItem;
-        Navigation.PushAsync(new Views.vActElim(objEstudiante));
+       //var objEstudiante = (Rol)e.SelectedItem;
+       // Navigation.PushAsync(new Views.vActElim(objEstudiante));
     }
 }
